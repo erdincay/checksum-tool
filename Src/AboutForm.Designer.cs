09009037f64cc3176dae215603c2828d1f50528b
Kimmo Varis <kimmov@gmail.com>
2008-05-08 08:30:34 +0000

    Fix the product name to about dialog. (Was without a space.)

--- Src/AboutForm.Designer.cs
+++ Src/AboutForm.Designer.cs
@@ -66,7 +66,7 @@ namespace CheckSumTool
         	this.label1.Name = "label1";
         	this.label1.Size = new System.Drawing.Size(89, 19);
         	this.label1.TabIndex = 1;
-        	this.label1.Text = "CheckSumTool";
+        	this.label1.Text = "CheckSum Tool";
         	// 
         	// label2
         	// 
