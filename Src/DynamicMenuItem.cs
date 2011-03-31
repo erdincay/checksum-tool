6b55975d377a8001961e9438c15822b5b187049f
Kimmo Varis <kimmov@gmail.com>
2011-03-31 20:51:47 +0300

    Compile fix.

--- Src/DynamicMenuItem.cs
+++ Src/DynamicMenuItem.cs
@@ -8,14 +8,12 @@ namespace CheckSumTool
 {
     public class DynamicMenuItem : MenuItem
     {
-        private string _data;
-
         // Dynamic menus information, i.e. menu text and menu data are
         // normally retrieved from some source like a database or
         // Registry. Putting them into an array of DynamicMenuItemTextData
         // struct will make it convenient to prepare for creating a group
         // of dynamic menu items through the dynamic menu manager.
-        public struct DynamicMenuItemTextData
+        public class DynamicMenuItemTextData
         {
             public string MenuText { get; set; }
             public string MenuData { get; set; }
